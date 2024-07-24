b3bf407c31d429e673a0018927a3fef8a317152b
Ian Mc Farlane <ian2002.11.09@gmail.com>
2024-07-24 14:05:12 -0600

    Reworked how the numbers get translated in the rotation to make it more picture friendly in the future

--- scripts/sliderPuzzleLogic/sliderPuzzleRotator.js
+++ scripts/sliderPuzzleLogic/sliderPuzzleRotator.js
@@ -23,22 +23,36 @@ class SliderPuzzleRotator extends SliderPuzzle {
     #setMaps() {
         // base
         const board = this._gameState;
+        const directions = ['up', 'left', 'down', 'right'];
         const size = this._size;
 
+        // No rotation => identity maps
+        if (this.#rotation === 'NONE') {
+            
+            // build maps
+            this.#rotate =  Object.fromEntries(
+                board.map(x => [x, x]).concat(directions.map(x => [x, x]))
+            )
+
+            this.#unrotate = Object.fromEntries(
+                board.map(x => [x, x]).concat(directions.map(x => [x, x]))
+            )
+
+            return;
+        }
+        
+        
         // initialize useful arrays
         const boardIndices = Array.from({length: size * size}, (_, i) => i);
         const directionIndices = [0, 1, 2, 3];
 
-
+        // arrays to be rotated
+        let rotatedDirections = ['up', 'left', 'down', 'right'];
         let rotatedBlockBoard = Array.from({length: size}, (_, i) => 
             board.slice(i * size, (i + 1) * size)
         );
 
-        const directions = ['up', 'left', 'down', 'right']
-        let rotatedDirections = ['up', 'left', 'down', 'right']
-
-
-        // find rotated array
+        // rotate arrays
         let rotationCount = ROTATIONS.indexOf(this.#rotation);
 
         for(let i = 0; i < rotationCount; i++) {
@@ -47,15 +61,27 @@ class SliderPuzzleRotator extends SliderPuzzle {
         }
 
         const rotatedBoard = rotatedBlockBoard.flat();
+        const zeroIndex = rotatedBoard.indexOf(0);
+        
+        // board but with size^2 instead of zero
+        const unzeroedBoard = boardIndices.map(i => i + 1);
+
+        // removes the element at the ith position and add a zero at the end
+        let syncer = (arr, i) => arr.slice(0,i).concat(arr.slice(i+1)).concat([0]);
+
+        // removes the element corresponding to the rotated zero's index
+        // and adds a zero at the end in both arrays 
+        const rot = syncer(rotatedBoard, zeroIndex);
+        const unrot = syncer(unzeroedBoard, zeroIndex);
 
         // build maps
         this.#rotate =  Object.fromEntries(
-            boardIndices.map(i => [board[i], rotatedBoard[i]])
+            boardIndices.map(i => [unrot[i], rot[i]])
                 .concat(directionIndices.map(i => [directions[i], rotatedDirections[i]])) 
         )
 
         this.#unrotate = Object.fromEntries(
-            boardIndices.map(i => [rotatedBoard[i], board[i]]) 
+            boardIndices.map(i => [rot[i], unrot[i]]) 
                 .concat(directionIndices.map(i => [rotatedDirections[i], directions[i]])) 
         )
     }
@@ -90,17 +116,11 @@ class SliderPuzzleRotator extends SliderPuzzle {
         }
 
         const table = blockBoard.map(row => {
-            const tr = row.map(x => `<td>${x}</td>`).join('\n');
+            const tr = row.map(x => `<td>${this.unrotate(x)}</td>`).join('\n');
             return `<tr>\n${tr}\n</tr>`
         }).join('\n');
 
         return `<table>\n${table}\n</table>`;
     }
 
-    toString(rotated=false) {
-        if(rotated)
-            return this._gameState.map(this.rotate).toString() 
-        
-        return this._gameState.toString() 
-    };
 }
